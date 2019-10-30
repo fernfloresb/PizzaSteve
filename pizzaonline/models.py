05fc5e7ff5a302939d930631caf65942d286f6fc
pizzasteve <fern.floresb@alumnos.duoc.cl>
2019-10-30 17:39:55 -0300

    modeloListo

--- pizzaonline/models.py
+++ pizzaonline/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+
+#Clase Ingredientes
+class Ingredients (models.Model)
+        ingredientName = models.Charfield(max_length=200)
+
+    def __str__(self):
+        return self.ingredientName
+
+#Clase Pizzas
+class Pizza (models.Model)
+    pizzaName = models.Charfield(max_length=200)
+    ingredient = models.ManyToManyField(Ingredients)
+    price = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.pizzaName
+
+#Clase Locales
+class store (models.Model)
+    localName = models.Charfield(max_length=200)
+    adress = models.Charfield(max_length=200)
+    phoneNumber = models.IntegerField(max_length=12)
+
+    def __str__(self):
+        return self.localName
+
+
+
+
+
